c7b7059a279764afdf57d7aea9f77c0ca5083de5
Kulik Kateryna <katerynakulik4@gmail.com>
2025-07-11 16:57:52 +0300

    2 tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,63 @@
+const formData = {
+    email: "", 
+    message: "", 
+};
+const form= document.querySelector(".feedback-form");
+const STORAGE_KEY = 'feedback-form-state';
+
+populateForm();
+
+form.addEventListener("input", onFormInput);
+
+form.addEventListener("submit", handleSubmit);
+
+
+function onFormInput(event){
+    const {name, value} = event.target;
+
+    if (name) {
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+};
+
+function populateForm() {
+    const savedData=localStorage.getItem(STORAGE_KEY);
+
+    if (savedData){
+        const parsedData = JSON.parse(savedData)
+        if (parsedData.email){
+            form.elements.email.value = parsedData.email;
+            formData.email = parsedData.email;
+        };
+        if (parsedData.message){
+            form.elements.message.value = savedData.message;
+            formData.message = parsedData.message;
+        }
+    }
+    
+};
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    
+
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+    
+    if (email==='' || message===''){
+    alert('All form fields must be filled in');
+    return;
+    };
+
+    console.log(`Submitted data:`, {email, message} );
+
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+
+    formData.email ='';
+    formData.message='';
+  
+};
